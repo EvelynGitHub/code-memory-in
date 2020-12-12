6d699f1e574a3808f2c59dc5811fc74628fa3ddf
EvelynGitHub <evelynbrandao15@gmail.com>
2020-12-12 15:11:23 -0300

    Tipos de comentários em Java

---
+++ Java/comentarios.java
@@ -0,0 +1,18 @@
+//Existente 3 tipos de comentários em java
+
+// 1º Este é o comentario de um linha
+
+/*
+  2º Este é o comentario de bloco,
+  ou seja, posso quebrar em mais de uma linha
+*/
+
+/*
+* Algumas pessoas colocam o * no inicio da linha
+* Mas é apenas uma questão Estécica do 2º tipo
+*/
+
+/** 
+  3º Este é um comentário de documentação (visto que ele tem ** na primeira linha)
+  Aqui poderia ser colocados os @param etc caso fosse necessário
+*/
