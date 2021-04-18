7b072af33a4f78541e07a1429956c6cd90513140
EvelynGitHub <evelynbrandao15@gmail.com>
2021-04-17 22:05:01 -0300

    Comentários em JS

---
+++ JS/comentarios.js
@@ -0,0 +1,10 @@
+/*
+Este é um comentário de bloco, para isso o conteudo deve estar dentro de barra asterisco asterisco barra.
+Este comentário pode ter mais de uma linha.
+*/
+
+// Este é um comentário de uma linha
+
+/**
+* Este também é um comentário de bloco, mas possui mais asterisos.
+*/
